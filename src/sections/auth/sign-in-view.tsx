ed130be93097e6161d25b5b867b3f7ec2551815b
tylerdesouza <tylercdesouza@gmail.com>
2025-04-19 08:59:39 -0400

    Updating styling

--- src/sections/auth/sign-in-view.tsx
+++ src/sections/auth/sign-in-view.tsx
@@ -1,42 +1,136 @@
-// src/sections/auth/SignInView.tsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-// Define the types for props
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputAdornment from '@mui/material/InputAdornment';
+
+import { Iconify } from 'src/components/iconify';
+
 interface SignInViewProps {
   onLogin: (email: string, password: string) => void;
   error: string | null;
 }
 
-const SignInView: React.FC<SignInViewProps> = ({ onLogin, error }) => {
+export function SignInView({ onLogin, error }: SignInViewProps) {
+  const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    onLogin(email, password); // Call onLogin when the form is submitted
+    onLogin(email, password);
   };
 
-  return (
-    <div>
-      <h2>Sign In</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <button type="submit">Sign In</button>
-      </form>
-      {error && <p>{error}</p>} {/* Display error message if there is one */}
-    </div>
-  );
-};
+  const renderForm = (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        display: 'flex',
+        alignItems: 'flex-end',
+        flexDirection: 'column',
+      }}
+    >
+      <TextField
+        fullWidth
+        name="email"
+        label="Email address"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        sx={{ mb: 3 }}
+        slotProps={{
+          inputLabel: { shrink: true },
+        }}
+      />
 
-export { SignInView };
+      <Link variant="body2" color="inherit" sx={{ mb: 1.5 }}>
+        Forgot password?
+      </Link>
+
+      <TextField
+        fullWidth
+        name="password"
+        label="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type={showPassword ? 'text' : 'password'}
+        slotProps={{
+          inputLabel: { shrink: true },
+          input: {
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                  <Iconify icon={showPassword ? 'solar:eye-bold' : 'solar:eye-closed-bold'} />
+                </IconButton>
+              </InputAdornment>
+            ),
+          },
+        }}
+        sx={{ mb: 3 }}
+      />
+
+      <Button fullWidth size="large" type="submit" color="inherit" variant="contained">
+        Sign in
+      </Button>
+
+      {error && (
+        <Typography variant="body2" color="error" sx={{ mt: 2 }}>
+          {error}
+        </Typography>
+      )}
+    </Box>
+  );
+
+  return (
+    <>
+      <Box
+        sx={{
+          gap: 1.5,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          mb: 5,
+        }}
+      >
+        <Typography variant="h5">Sign in</Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Don’t have an account?
+          <Link variant="subtitle2" sx={{ ml: 0.5 }}>
+            Get started
+          </Link>
+        </Typography>
+      </Box>
+      {renderForm}
+      <Divider sx={{ my: 3, '&::before, &::after': { borderTopStyle: 'dashed' } }}>
+        <Typography
+          variant="overline"
+          sx={{ color: 'text.secondary', fontWeight: 'fontWeightMedium' }}
+        >
+          OR
+        </Typography>
+      </Divider>
+      <Box
+        sx={{
+          gap: 1,
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <IconButton color="inherit">
+          <Iconify width={22} icon="socials:google" />
+        </IconButton>
+        <IconButton color="inherit">
+          <Iconify width={22} icon="socials:github" />
+        </IconButton>
+        <IconButton color="inherit">
+          <Iconify width={22} icon="socials:twitter" />
+        </IconButton>
+      </Box>
+    </>
+  );
+}
